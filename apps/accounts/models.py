b4d86cb03f53c343219d243e51071e2f60fe250a
rogeriodelphi <rogeriodelphi@gmail.com>
2022-02-27 02:19:24 -0300

    Refatorando o Perfil do Usuário

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,15 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from apps.core.funcoes import sexo
+from apps.core.funcoes import sexo, estados
 
 
 class UserProfile(models.Model):
 
     SX_CHOICES = sexo.choices_sexo()
+    UF_CHOICES = estados.choices_estados()
 
     photo = models.ImageField('Foto', upload_to='photos')
-    cel_phone = models.CharField('Celular', max_length=16)
 
     data_nascimento = models.DateField(verbose_name='Data de Nascimento', null=True)
     sexo = models.CharField(max_length=1, choices=SX_CHOICES, default="", blank=True, null=True)
@@ -19,6 +19,11 @@ class UserProfile(models.Model):
     numero = models.CharField(max_length=6, verbose_name="Número", blank=True, null=True)
     complemento = models.CharField(max_length=60, verbose_name="Complemento", null=True, blank=True)
     bairro = models.CharField(max_length=50, verbose_name="Bairro", null=True, blank=True)
+
+    cidade = models.CharField(max_length=30, verbose_name="Cidade", null=True, blank=True)
+    estado = models.CharField(max_length=2, choices=UF_CHOICES, default="BA", verbose_name="Estado")
+    cep = models.CharField(max_length=9, verbose_name="CEP", null=True, blank=True)
+    cel_phone = models.CharField('Celular', max_length=16)
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
--- staticfiles/js/mask.js
+++ staticfiles/js/mask.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
     $('#id_cel_phone').mask('(00) 00000-0000');
+    $('#id_cpf').mask('000.000.000-00');
+    $('#id_cep').mask('00000-000');
 });
